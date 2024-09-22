76ad016872dd215c0e8496c7326266ee1b650e76
ehsan <ehsan02032014@gmail.com>
2024-09-22 09:45:25 +0330

    Fixed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,9 @@ import { SelectLessonComponent } from './select-lesson/select-lesson.component';
 import { QuestionPageComponent } from './question-page/question-page.component';
 import { ResultComponent } from './result/result.component';
 import { AuthGuard } from './auth.guard'; // گاردی که ساخته‌ایم
+import { SelectionPageComponent } from './selection-page/selection-page.component';
+import { EducationPageComponent } from './education-page/education-page.component';
+import { LessonDetailsComponent } from './lesson-details/lesson-details.component';
 
 export const routes: Routes = [
     { path: '', component: PhoneInputComponent, canActivate: [AuthGuard] }, // بررسی با AuthGuard
@@ -12,4 +15,8 @@ export const routes: Routes = [
     { path: 'select-lesson', component: SelectLessonComponent },
     { path: 'question', component: QuestionPageComponent },
     { path: 'result', component: ResultComponent },
+    { path: 'select-page', component: SelectionPageComponent },
+    { path: 'education', component: EducationPageComponent },
+    { path: 'lesson-details/:lessonNumber/:level', component: LessonDetailsComponent },
+    
 ];
--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -13,7 +13,7 @@ export class AuthGuard implements CanActivate {
     // اگر کاربر لاگین کرده و UserId موجود است
     if (userId) {
       // هدایت به صفحه "select-lesson"
-      this.router.navigate(['/select-lesson']);
+      this.router.navigate(['/select-page']);
       return false; // اجازه نمی‌دهد که کاربر به صفحه اصلی دسترسی پیدا کند
     }
 
